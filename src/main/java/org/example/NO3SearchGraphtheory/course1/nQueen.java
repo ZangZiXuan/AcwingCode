e76965397c103e136852ba5201b6ba8d838ba88e
王山而 <346669054@qq.com>
2024-04-24 16:04:55 +0800

    N皇后问题

---
+++ src/main/java/org/example/NO3SearchGraphtheory/course1/nQueen.java
@@ -0,0 +1,53 @@
+package org.example.NO3SearchGraphtheory.course1;
+
+import java.util.Scanner;
+
+/**
+ * @Author Zang Xinrui
+ * @Description TODO
+ * @Date 2024/4/24 13:29
+ * @Version 1.0
+ */
+public class nQueen {
+
+    static int N = 20;
+    static int n;
+    static char[][] g = new char[N][N];
+    static boolean[] col =  new boolean[N];
+    static boolean[] dg =  new boolean[N];
+    static boolean[] udg =  new boolean[N];
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < n ;j++){
+                g[i][j] =  '`'; // 设置默认值
+            }
+        }
+        dfs(0);
+    }
+    static void dfs(int u){
+        if(u == n){
+            for(int i = 0; i < n; i++){
+                for(int j = 0; j < n;j++){
+                    if(!String.valueOf(g[i][j]).equals("")){
+                        System.out.print(g[i][j]);
+                        System.out.print(" ");
+                    }
+                }
+                System.out.println();
+            }
+            return;
+        }
+        for(int i = 0; i < n; i++){
+            if(!col[i] && !dg[u + i] && !udg[n - u + i]){
+                g[u][i] = 'Q';
+                col[i] = dg[u + i] = udg[n - u + i] = true;
+                dfs(u+1);
+                col[i] = dg[u + i] = udg[n - u + i] = false;
+                g[u][i] = '`';
+            }
+        }
+    }
+}
